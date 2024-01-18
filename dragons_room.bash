630805b284c0ecf78a387191ef51d739109c5214
Peach-Jelly <91342661+Peach-Jelly@users.noreply.github.com>
2024-01-18 16:12:26 -0500

    Add files via upload

---
+++ dragons_room.bash
@@ -0,0 +1,96 @@
+
+
+#Dragon's room
+sleep 1.9
+echo As you walk cautiously down the corridor, you hear a low rumble...
+echo  
+sleep 1.9
+echo "== !!! A dragon emerges !!! =="
+echo  
+sleep 1.9
+if [ $has_sword == yes ]; then
+echo You unsheathe your mighty sword and make swift business of this dragon!
+echo  
+sleep 1.9
+echo You find his treasure and emerge victorious.
+echo  
+sleep 1.3
+echo The end.
+echo  
+sleep 1
+echo Thanks for playing!
+echo  
+sleep 1
+echo Play again?
+sleep 0.5
+echo "(1) Yes, (2) No"
+read playagain
+if [ $playagain = 1 ]; then
+/bin/bash game.bash
+else
+echo  
+echo Goodbye!
+fi
+else
+echo "(1) Use your sword!"
+echo "(2) Oh... shit."
+read lulz
+if [ $lulz == 1 ]; then
+echo  
+echo Nope.
+echo  
+sleep 1
+echo The dragon eats you.
+echo  
+sleep 1
+echo The end.
+echo  
+sleep 1
+echo Try again?
+echo  
+echo "(1) Yes, (2) No"
+read retry_a
+if [ $retry_a == 1 ]; then
+/bin/bash game.bash
+elif [ $retry_a == 2 ]; then
+echo  
+echo Thanks for playing! Byeeee
+fi
+elif [ $lulz == 2 ]; then
+echo  
+echo The dragon eats you.
+echo  
+sleep 1
+echo The end.
+echo  
+sleep 1
+echo Try again?
+echo  
+echo "(1) Yes, (2) No"
+read retry
+if [ $retry == 1 ]; then
+/bin/bash game.bash
+elif [ $retry == 2 ]; then
+echo  
+echo Thanks for playing! Byeeee
+fi
+else
+echo "You're not very good at this, are you?..."
+echo  
+sleep 1.3
+echo The dragon eats you.
+echo  
+sleep 1
+echo The end.
+echo  
+sleep 1
+echo Try again?
+echo "(1) Yes, (2) No"
+read retry_b
+if [ $retry_b == 1 ]; then
+/bin/bash game.bash
+elif [ $retry_b == 2 ]; then
+echo Thanks for playing! Byeeee
+fi
+fi
+fi
---
+++ game.bash
@@ -0,0 +1,48 @@
+echo  
+echo "~~ Welcome to the world's shortest adventure game. ~~"
+echo  
+sleep 1.7
+echo \"It\'s dangerous to go alone! Take this.\"
+echo  
+sleep 1.3
+echo The old wizard extends the hilt of a sword toward you...
+echo  
+sleep 1.3
+echo "(1) Take it."
+echo "(2) Tell the old man to piss off."
+while [ "$failed_input" != "pass" ];
+do
+read sword_deci
+if [ $sword_deci == 1 ]; then
+echo  
+echo "You take the sword..."
+echo  
+sleep 1.3
+export has_sword=yes
+echo You bid the wizard a thankful farewell as you venture toward the dungeon...
+echo  
+failed_input=pass
+/bin/bash dragons_room.bash
+elif [ $sword_deci == 2 ]; then
+echo  
+echo '"My word!," the wizard gasps. "You are not worthy!" ...'
+echo  
+export has_sword=no
+sleep 1.7
+echo He then vanishes in a puff of orange smoke...
+echo  
+sleep 1.7
+echo '"This place blows," you say as you leave the entry '
+echo 'and make your way down the hall...'
+echo  
+sleep 1
+failed_input=pass
+/bin/bash dragons_room.bash
+else 
+echo "Please enter 1 or 2."
+failed_input=fail
+fi
+done
+
+
+
